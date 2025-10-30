07ec679022dac31ad8520f544a4c2a84cac0f0cc
Nagendra1181 <nag@gmail.com>
2025-10-30 16:21:48 +0530

    commiting the file

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 touch file1.txt
-echo " hi everyone" >> file1.txt
+echo " hai everyone  today topic is automating the task using the bash file" >> file1.txt
 git add .
 git commit -m "commiting the file" 
 cat file1.txt
